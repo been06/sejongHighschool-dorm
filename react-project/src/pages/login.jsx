740d75bbde6eb76e5e8adf014d8e7401b84df7ee
been06 <hyungbeenjo@naver.com>
2024-03-26 16:17:22 +0900

    create simple login pages

---
+++ react-project/src/pages/login.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function AboutPage() {
+  return (
+    <div class="login">
+      <input type="text" placeholder="Id" />
+      <input type="text" placeholder="password" />
+    </div>
+  );
+}
+
+export default AboutPage;
--- server.js
+++ server.js
@@ -3,5 +3,11 @@ const path = require('path');
 const app = express();
 
 app.listen(8080, function () {
-  console.log('listening on 8080')
-}); 
+  console.log('listening on 8080');
+});
+
+app.use(express.static(path.join(__dirname, 'react-project/build')));
+
+app.get('/', function (요청, 응답) {
+  응답.sendFile(path.join(__dirname, '/react-project/build/index.html'));
+});
