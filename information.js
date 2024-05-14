bd4489b08b7f772d6bbae491a2f572d0bee83a94
Jin__Yee <142388279+kjinyeek@users.noreply.github.com>
2024-05-14 16:08:36 +0900

    Add files via upload

--- information.js
+++ information.js
@@ -1,9 +1,17 @@
 const xlsx = require('xlsx');
 
-const workbook = xlsx.readFile('./student_imformation.xlsx'); // 액샐 파일 읽어오기
+const workbook = xlsx.readFile('./student_information.xlsx'); //액셀 파일 가져오기
 const firstSheetName = workbook.SheetNames[0]; // 첫 번째 시트 이름 가져오기
-const firstShee = workbook.Sheets[firstSheetName]; // 시트 이름을 이용해 엑셀 파일의 첫 번째 시트 가져오기
+const firstShee = workbook.Sheets[firstSheetName]; // 첫  번째 시트 가져오기
+const secondSheetName = workbook.SheetNames[0]; // 두 번째 시트 이름 가져오기
+const secondShee = workbook.Sheets[secondSheetName]; // 두 번째 시트 가져오기
+const thirdSheetName = workbook.SheetNames[0]; // 세 번째 시트 이름 가져오기
+const thirdShee = workbook.Sheets[thirdSheetName]; // 세 번째 시트 가져오기
 
 const firstSheeJson = xlsx.utils.sheet_to_json(firstShee); // 첫 번째 시트 내용을 json 데이터로 변환
+const secondSheeJson = xlsx.utils.sheet_to_json(secondShee); // 두 번째 시트 내용을 json 데이터로 변환
+const thirdSheeJson = xlsx.utils.sheet_to_json(thirdShee); // 세 번째 시트 내용을 json 데이터로 변환
 
 console.log(firstSheeJson);
+console.log(secondSheeJson);
+console.log(thirdSheeJson); 
