5db358f3c1d2f718b92f5d4c1e6ae0878dce4fa1
Jin__Yee <142388279+kjinyeek@users.noreply.github.com>
2024-05-14 16:18:44 +0900

    Update information.js

--- information.js
+++ information.js
@@ -1,11 +1,11 @@
 const xlsx = require('xlsx');
 
 const workbook = xlsx.readFile('./student_information.xlsx'); //액셀 파일 가져오기
-const firstSheetName = workbook.SheetNames[0]; // 첫 번째 시트 이름 가져오기
+const firstSheetName = workbook.SheetNames[0]; // 첫 번째 시트 이름 가져오기-1학년
 const firstShee = workbook.Sheets[firstSheetName]; // 첫  번째 시트 가져오기
-const secondSheetName = workbook.SheetNames[0]; // 두 번째 시트 이름 가져오기
+const secondSheetName = workbook.SheetNames[0]; // 두 번째 시트 이름 가져오기-2학년
 const secondShee = workbook.Sheets[secondSheetName]; // 두 번째 시트 가져오기
-const thirdSheetName = workbook.SheetNames[0]; // 세 번째 시트 이름 가져오기
+const thirdSheetName = workbook.SheetNames[0]; // 세 번째 시트 이름 가져오기-3학년
 const thirdShee = workbook.Sheets[thirdSheetName]; // 세 번째 시트 가져오기
 
 const firstSheeJson = xlsx.utils.sheet_to_json(firstShee); // 첫 번째 시트 내용을 json 데이터로 변환
